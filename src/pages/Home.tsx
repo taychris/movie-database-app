9f0334d195fcf3838528946a93362213687ad330
Christopher Szabo <tay.christopha@gmail.com>
2023-10-03 16:32:03 +0200

    added page titles, movie card fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,92 +3,11 @@ import MovieList from "@/components/home/MovieList";
 import SearchBar from "@/components/home/SearchBar";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   const [searchText, setSearchText] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
-  // const [movieList, setMovieList] = useState([
-  //   {
-  //     Title: "Batman Begins",
-  //     Year: "2005",
-  //     imdbID: "tt0372784",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman v Superman: Dawn of Justice",
-  //     Year: "2016",
-  //     imdbID: "tt2975590",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "The Batman",
-  //     Year: "2022",
-  //     imdbID: "tt1877830",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman",
-  //     Year: "1989",
-  //     imdbID: "tt0096895",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman Returns",
-  //     Year: "1992",
-  //     imdbID: "tt0103776",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman & Robin",
-  //     Year: "1997",
-  //     imdbID: "tt0118688",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BMGQ5YTM1NmMtYmIxYy00N2VmLWJhZTYtN2EwYTY3MWFhOTczXkEyXkFqcGdeQXVyNTA2NTI0MTY@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman Forever",
-  //     Year: "1995",
-  //     imdbID: "tt0112462",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "The Lego Batman Movie",
-  //     Year: "2017",
-  //     imdbID: "tt4116284",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman: The Animated Series",
-  //     Year: "1992–1995",
-  //     imdbID: "tt0103359",
-  //     Type: "series",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_SX300.jpg",
-  //   },
-  //   {
-  //     Title: "Batman v Superman: Dawn of Justice (Ultimate Edition)",
-  //     Year: "2016",
-  //     imdbID: "tt18689424",
-  //     Type: "movie",
-  //     Poster:
-  //       "https://m.media-amazon.com/images/M/MV5BOTRlNWQwM2ItNjkyZC00MGI3LThkYjktZmE5N2FlMzcyNTIyXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg",
-  //   },
-  // ]);
 
   function searchMovies(e: React.FormEvent) {
     e.preventDefault();
@@ -105,17 +24,22 @@ const Home = () => {
   }, [searchParams]);
 
   return (
-    <section className="space-y-5">
-      <div className="flex w-full gap-5">
-        <SearchBar
-          searchMovies={searchMovies}
-          searchText={searchText}
-          setSearchText={setSearchText}
-        />
-        <FavoritesButton/>
-      </div>
-      <MovieList />
-    </section>
+    <>
+      <Helmet>
+        <title>Home - Movie Database</title>
+      </Helmet>
+      <section className="space-y-5">
+        <div className="flex w-full gap-5">
+          <SearchBar
+            searchMovies={searchMovies}
+            searchText={searchText}
+            setSearchText={setSearchText}
+          />
+          <FavoritesButton/>
+        </div>
+        <MovieList />
+      </section>
+    </>
   );
 };
 export default Home;
--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -7,68 +7,80 @@ import Ratings from "@/components/movie/Ratings";
 import { Separator } from "@/components/ui/separator";
 import { loadMovieDetails } from "@/services/apiCalls";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 
 const Movie = () => {
+  const [title, setTitle] = useState("");
   const { imdbID } = useParams();
-  const {data, isLoading, isSuccess} = useQuery({
+  const { data, isLoading, isSuccess } = useQuery({
     queryKey: ["movie-detail", imdbID],
-    queryFn: async () => loadMovieDetails(imdbID || "")
-  })
+    queryFn: async () => loadMovieDetails(imdbID || ""),
+  });
+
+  useEffect(() => {
+    if (data) setTitle(data.Title);
+  }, [data]);
 
   return (
-    <section className="flex flex-col gap-5 text-left">
-      <div className="flex justify-between w-full gap-5">
-        <BackButton />
-        <FavoritesButton />
-      </div>
-      {isLoading && <p>Loading...</p>}
-      {isSuccess && 
-      <>
-        <div className="grid gap-5 md:grid-cols-2">
-          <Heading
-            Title={data.Title}
-            imdbID={data.imdbID}
-            Year={data.Year}
-            Runtime={data.Runtime}
-            Poster={data.Poster}
-            Released={data.Released}
-            Genre={data.Genre}
-            Rated={data.Rated}
-          />
-          <Ratings rating={data.Ratings} votes={data.imdbVotes}/>
+    <>
+      <Helmet>
+        <title>{title} - Movie Database</title>
+      </Helmet>
+      <section className="flex flex-col gap-5 text-left">
+        <div className="flex justify-between w-full gap-5">
+          <BackButton />
+          <FavoritesButton />
         </div>
-        <div className="grid gap-5 md:grid-cols-2">
-          <div className="flex flex-col gap-5 lg:flex-row">
-            <img
-              src={data.Poster}
-              className="w-full max-w-xs rounded-xl"
-              alt={`${data.Title} poster`}
-            />
-            <MovieDetails
-              Language={data.Language}
-              Country={data.Country}
-              Awards={data.Awards}
-              BoxOffice={data.BoxOffice}
-              Production={data.Production}
-              DVD={data.DVD}
-              Type={data.Type}
-              Website={data.Website}
-            />
-          </div>
-          <div className="space-y-4">
-            <p className="text-lg font-light">{data.Plot}</p>
-            <Separator />
-            <Cast
-              Director={data.Director}
-              Writer={data.Writer}
-              Actors={data.Actors}
-            />
-          </div>
-        </div>
-      </>
-      }
-    </section>
+        {isLoading && <p>Loading...</p>}
+        {isSuccess && (
+          <>
+            <div className="grid gap-5 md:grid-cols-2">
+              <Heading
+                Title={data.Title}
+                imdbID={data.imdbID}
+                Year={data.Year}
+                Runtime={data.Runtime}
+                Poster={data.Poster}
+                Released={data.Released}
+                Genre={data.Genre}
+                Rated={data.Rated}
+              />
+              <Ratings rating={data.Ratings} votes={data.imdbVotes} />
+            </div>
+            <div className="grid gap-5 md:grid-cols-2">
+              <div className="flex flex-col gap-5 lg:flex-row">
+                <img
+                  src={data.Poster}
+                  className="w-full max-w-xs rounded-xl"
+                  alt={`${data.Title} poster`}
+                />
+                <MovieDetails
+                  Language={data.Language}
+                  Country={data.Country}
+                  Awards={data.Awards}
+                  BoxOffice={data.BoxOffice}
+                  Production={data.Production}
+                  DVD={data.DVD}
+                  Type={data.Type}
+                  Website={data.Website}
+                />
+              </div>
+              <div className="space-y-4">
+                <p className="text-lg font-light">{data.Plot}</p>
+                <Separator />
+                <Cast
+                  Director={data.Director}
+                  Writer={data.Writer}
+                  Actors={data.Actors}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </section>
+    </>
   );
 };
 export default Movie;
